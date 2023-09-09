ef9a196451eacd0cd19a04a373c99863275de353
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-09-08 22:41:25 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - entry point
  *
- * Description: false or true validation 
+ * Description: false or true validation
  *
- * Return: 0 (success) 
+ * Return: 0 (success)
 */
 
 int main(void)
