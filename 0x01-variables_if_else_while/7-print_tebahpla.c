6c957bb14ea5991f385f880f7b852a5fc244b9a1
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-09-09 23:47:54 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,38 +6,22 @@
  * Description: print the alphabet letters in reverse
  *
  * Return: Always (0) Success
-<<<<<<< HEAD
  *
-=======
- * 
->>>>>>> cbb349b7ef92ae3d36919364eab62f30edc5a734
+ *
 */
 
 int main(void)
 {
-<<<<<<< HEAD
-        char ch = 'z';
 
-        while (ch >= 'a')
-        {
-                putchar(ch);
-                ch--;
-        }
+	char ch = 'z';
 
-        putchar('\n');
-
-        return (0);
-=======
-	int ch = 128;
-
-	while (ch >= 97)
+	while (ch >= 'a')
 	{
 		putchar(ch);
 		ch--;
-	}	
+	}
 
 	putchar('\n');
-	
+
 	return (0);
->>>>>>> cbb349b7ef92ae3d36919364eab62f30edc5a734
 }
