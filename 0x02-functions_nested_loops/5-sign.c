68532470bd023656dae4fd12ae51e14adb2ca664
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-09-13 01:51:46 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,15 +17,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_Putchar(43);
-		 return (1);
+		return (1);
 	}
 	else if (n == 0)
-	{	
+	{
 		_putchar(48);
 		return (0);
 	}
 	else
-	{	
+	{
 		_putchar(45);
 		return (-1);
 	}
