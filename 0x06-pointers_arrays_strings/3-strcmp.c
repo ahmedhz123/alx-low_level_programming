1511710ee109abe4187c221c51a2cce89c7f993a
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-09-20 21:49:06 -0700

    2

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function to compare to string
+ *
+ * @s1: the pointer parameter
+ * @s2: the pointer parameter
+ *
+ * Return: 0 Always (Success)
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
