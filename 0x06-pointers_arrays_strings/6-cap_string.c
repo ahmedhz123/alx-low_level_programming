72ca72c300a7b915e1c7e9709e3ce394560dce3c
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-09-21 04:54:55 -0700

    2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
 	char sp[13] = {',', ';', '.', '"', '(', ')', '{', '}'
 		, '!', '?', ' ', '\t', '\n'};
 
@@ -19,7 +19,7 @@ char *cap_string(char *s)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
-		for (int j = 0; j < 13; j++)
+		for (j = 0; j < 13; j++)
 		{
 			if (s[i] == sp[j])
 			{
