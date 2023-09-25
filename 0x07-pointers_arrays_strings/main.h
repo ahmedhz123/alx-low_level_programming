62a61e172f818e2d7e4e3f4fa012704a06a341ae
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-09-25 11:54:07 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef "MAIN_H"
 #define "MAIN_H"
 
+int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
