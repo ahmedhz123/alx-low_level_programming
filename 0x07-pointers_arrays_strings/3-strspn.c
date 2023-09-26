f84f9b798a40f9649f0f06a4472225b91dbd90f5
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-09-25 22:32:35 -0700

    s

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,12 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, n = 0;
+	int i, j;
 
-	for (i = 0; *s != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; s[i] != accept[j]; j++)
-			if (accept[j == '\0'])
+			if (accept[j] == '\0')
 				return (i);
 	}
 	return (0);
