5e57720a5f295e2d88f489bade12528299c80830
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-10-02 13:27:43 -0700

    s

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: the arguments count
+ * @argv: the arguments vector
+ *
+ * Return: the multiplication of two numbers
+*/
+
+int main(int argc, char *argv)
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul  = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
