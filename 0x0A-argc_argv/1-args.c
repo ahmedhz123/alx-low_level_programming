3d64bda621a02ee89aa06dc440dcf40112b4f537
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-10-02 12:55:04 -0700

    sd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ *
+ * @argc: the arguments count
+ * @argv: the arguments vector
+ *
+ * Return: 0 Always (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
