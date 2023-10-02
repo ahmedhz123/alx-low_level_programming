577a424d52a4983f698db6472ca86bfc18fb578a
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-10-02 13:11:44 -0700

    s

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - prints the contents of the argv array
  *
+ * @argc: the arguments count
+ * @argv: the arguments vector
  *
- *
- * */
+ * Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
