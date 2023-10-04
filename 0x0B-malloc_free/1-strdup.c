9740681a2af5a1dd9ef8eef315c7df2e82e79221
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-10-03 21:39:17 -0700

    sad

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	copy = malloc(sizeof(char) * (count + 1));
 
