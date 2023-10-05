832a356acb3cfdfe110ca8a4dc8fe46953b6280e
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-10-05 04:22:23 -0700

    d

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,6 +18,9 @@ char *argstostr(int ac, char **av)
 	int count = 0;
 	char *output;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
