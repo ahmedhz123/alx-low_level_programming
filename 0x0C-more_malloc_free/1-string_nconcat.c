9d9d25421231c367c16a32438b143976449c8c90
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-10-07 11:00:50 -0700

    sd

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		for (i = 0; *s1 != '\0'; i++)
 			output[i] = s1[i];
-		for (i = 0; s2[i] != '\0'; i++)
+		for (i = 0; *s2[i] != '\0'; i++)
 			output[length1 + i] = s2[i];
 		output[length1 + i] = '\0';
 	}
