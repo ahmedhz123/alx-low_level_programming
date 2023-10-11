676da51fffa0b553ccf7f0ce48c48807dfd0fc4e
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-10-11 04:53:18 -0700

    sd

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * new_dog: creates new dog
+ *
+ * @name: the name of the new dog
+ * @age: the age of the new dog
+ * @owner: the onwer of the new dog
+ *
+ * Retunr: the new dog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t newDog;
+
+	newDog->name = name;
+	newDog->age = age;
+	newDog->owner = owner;
+
+	if (newDog == NULL)
+		return (NULL);
+	return (newDog);
+}
