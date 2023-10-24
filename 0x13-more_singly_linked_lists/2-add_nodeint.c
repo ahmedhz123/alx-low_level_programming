19a6ec43aae5a032c5c0d2ff5b37c25614949c4c
ahmedhz123 <ahmedhamzahammer123@gmail.com>
2023-10-24 00:07:08 -0700

    sd

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,13 +17,15 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (head == NULL || newnode == NULL)
 	{
-		newnode->next = NULL;
-		newnode->n = n;
+		return (NULL);
 	}
+	
+	newnode->next = NULL;
+	newnode->n = n;
+
 	if (*head)
 	{
 		newnode->next = *head;
-		newnode->n = n;
 	}
 	*head = newnode;
 	return (newnode);
