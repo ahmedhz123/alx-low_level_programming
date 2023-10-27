ac724e543fdf327635863d143e5ea2f0a4ae093a
ahmedhz123 <ahmedhamzahammer123@gmail.com>
2023-10-27 11:27:37 -0700

    sd

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - clear the bit and set it 0
+ *
+ * @n: the pointer to the bin number
+ * @index: the index of the bit in the given number
+ *
+ * Return: 1 if success -1 if failure
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	if (*n & 1L << index)
+		n ^= 1L << index;
+	return (1);
+}
